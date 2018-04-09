a5d29461142ffa107fb67b5d04a77bae1ec96216
juanma27719 <juanma27719@gmail.com>
2018-04-09 09:46:54 -0500

    Agregando servicios del sidemenu

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -3,6 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SidebarService {
 
+  menu: any = [
+    { titulo: 'Principal', icono: 'mdi mdi-gauge',
+      submenu: [
+        {titulo: 'Dasboard', url: '/dashboard'},
+        {titulo: 'ProgressBar', url: '/progress'},
+        {titulo: 'Graficas', url: '/graficas1'}        
+      ]  
+    }
+  ];
+
   constructor() { }
 
 }
