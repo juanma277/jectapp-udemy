51434c4828e8761358600e58534b7f073a136b4d
juanma27719 <juanma27719@gmail.com>
2018-04-17 11:36:26 -0500

    Seccion 16 Terminada

--- src/app/pages/vehiculos/vehiculo.component.ts
+++ src/app/pages/vehiculos/vehiculo.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Ruta } from '../../models/ruta.model';
+import { Vehiculo } from '../../models/vehiculo.model';
 import { RutaService } from '../../services/ruta/ruta.service';
+import { VehiculoService } from '../../services/vehiculo/vehiculo.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ModalUploadService } from '../../components/modal-upload/modal-upload.service';
 
 @Component({
   selector: 'app-vehiculo',
@@ -11,20 +15,70 @@ import { RutaService } from '../../services/ruta/ruta.service';
 export class VehiculoComponent implements OnInit {
 
   rutas: Ruta[] = [];
+  vehiculo: Vehiculo = new Vehiculo('','','','','');
+  ruta: Ruta = new Ruta('');
+  textoAccion: string = 'crear un vehiculo';
 
-  constructor(public rutaService: RutaService) { }
+  constructor(public rutaService: RutaService,
+              public vehiculoService: VehiculoService,
+              public router: Router,
+              public activatedRoute: ActivatedRoute,
+              public modalUploadService: ModalUploadService) { 
+    
+                activatedRoute.params.subscribe(params =>{
+                  let id = params['id'];
+                  if(id !== 'nuevo'){
+                    this.cargarVehiculo(id);
+                    this.textoAccion = 'editar los datos del vehiculo';
+                  }
+                });
+
+
+              }
 
   ngOnInit() {
     this.rutaService.cargarRutas()
         .subscribe((resp:any)=>{
           this.rutas = resp.rutas;
         });
+    this.modalUploadService.notificacion
+        .subscribe(resp =>{
+          this.vehiculo.img = resp.vehiculo.img;
+        });
   }
 
-  guardarMedico(forma: NgForm){
-    console.log(forma.valid);
-    console.log(forma.value);
+  cargarVehiculo(id: string){
+    this.vehiculoService.cargarMedico(id)
+        .subscribe(vehiculo => {
+          this.vehiculo = vehiculo;
+          this.vehiculo.ruta = vehiculo.ruta._id;
+          this.cambioRuta(this.vehiculo.ruta);
+        });
+  }
+
+  guardarVehiculo(forma: NgForm){
+    if(forma.invalid){
+      return;
+    }
+
+    this.vehiculoService.guardarVehiculo(this.vehiculo)
+        .subscribe(vehiculo=>{
+          this.vehiculo._id = vehiculo._id;
+          this.router.navigate(['/vehiculo', vehiculo._id]);
+        });
     
+  }
+
+  cambiarFoto(){
+    this.modalUploadService.mostrarModal('Vehiculos', this.vehiculo._id);
+  }
+
+  cambioRuta(id: string){
+    if(id === ''){
+      return;
+    }
+    this.rutaService.obtenerRuta(id)
+        .subscribe(ruta=> this.ruta = ruta);
   }
 
 }
--- src/app/services/vehiculo/vehiculo.service.ts
+++ src/app/services/vehiculo/vehiculo.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { URL_SERVICIOS } from '../../config/config';
 import { UsuarioService } from '../usuario/usuario.service';
+import { Vehiculo } from '../../models/vehiculo.model';
 
 @Injectable()
 export class VehiculoService {
@@ -36,6 +37,37 @@ export class VehiculoService {
 
     return this.http.delete(url)
                .map((resp:any) => swal('Vehiculo Eliminado', 'Vehiculo eliminado correctamente', 'success'));
+  }
+
+  cargarMedico(id: string){
+    let url = URL_SERVICIOS + '/vehiculo/' + id;
+    return this.http.get(url)
+        .map((resp:any) => resp.vehiculo);  
+  }
+
+  guardarVehiculo(vehiculo: Vehiculo){
+    let url = URL_SERVICIOS + '/vehiculo/';  
+
+    if (vehiculo._id){
+      //Actualizando
+      url += '/' +vehiculo._id;
+      url += '?token=' + this.usuarioService.token;     
+
+      return this.http.put(url, vehiculo)
+                .map((resp:any) => {
+                  swal('Vehiculo actualizado', 'El vehiculo '+vehiculo.nombre+' ha sido actualizado.', 'success');
+                  return resp.vehiculo;          
+                });
+    }else{
+      //Creando
+      url += '?token=' + this.usuarioService.token;
+      return this.http.post(url, vehiculo)
+        .map((resp:any) => {
+          swal('Vehiculo creado', 'El vehiculo '+vehiculo.nombre+' ha sido creado.', 'success');
+          return resp.vehiculo;
+        });
+    }
+    
   }
 
 }
